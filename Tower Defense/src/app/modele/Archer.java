0a684b631caeea5674ef831edbc36bc7795f7fa1
Zmahoutondji <62448623+Zmahoutondji@users.noreply.github.com>
2020-05-21 21:50:58 +0200

    Add files via upload

---
+++ Tower Defense/src/app/modele/Archer.java
@@ -0,0 +1,21 @@
+package app.modele;
+
+import javafx.scene.image.ImageView;
+
+public class Archer extends Tour {
+
+	public Archer(String nomDefense, int p, int a, int x, int y) {
+		super("Archer", 400, 40, x, y);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void agit() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	}
+	
+
+
