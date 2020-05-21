f1bd40191262f51bdaed5c975db090e2f8541b1d
Zmahoutondji <62448623+Zmahoutondji@users.noreply.github.com>
2020-05-21 21:53:55 +0200

    Add files via upload

---
+++ Tower Defense/src/app/modele/Mage.java
@@ -0,0 +1,17 @@
+package app.modele;
+
+public class Mage extends Tour {
+
+	public Mage(String nomDefense, int p, int a, int x, int y) {
+		super("Mage", 600, 4, x, y);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void agit() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
