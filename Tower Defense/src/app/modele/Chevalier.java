9cdb5f30ed7964352c7fb6dc322dcb75b0879e69
Zmahoutondji <62448623+Zmahoutondji@users.noreply.github.com>
2020-05-21 21:52:57 +0200

    Add files via upload

---
+++ Tower Defense/src/app/modele/Chevalier.java
@@ -0,0 +1,16 @@
+package app.modele;
+
+public class Chevalier extends Tour{
+
+	public Chevalier(String nomDefense, int p, int a, int x, int y) {
+		super("Chevalier", 500, 60, x, y);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void agit() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
