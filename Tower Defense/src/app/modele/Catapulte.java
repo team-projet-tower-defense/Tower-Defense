cc76a65bf05930f70e0b02eef6c55b08a87dbc2c
Zmahoutondji <62448623+Zmahoutondji@users.noreply.github.com>
2020-05-21 21:51:51 +0200

    Add files via upload

---
+++ Tower Defense/src/app/modele/Catapulte.java
@@ -0,0 +1,16 @@
+package app.modele;
+
+public class Catapulte extends Tour {
+
+	public Catapulte(String nomDefense, int p, int a, int x, int y) {
+		super("Catapulte", 300, 30, x, y);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void agit() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
