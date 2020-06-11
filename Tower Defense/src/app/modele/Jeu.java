ea2a3eea6d24b0a5e84022dc976dd714b2b0e209
ilyes-annou <63000921+ilyes-annou@users.noreply.github.com>
2020-06-11 18:45:41 +0200

    Update Jeu.java

--- Tower Defense/src/app/modele/Jeu.java
+++ Tower Defense/src/app/modele/Jeu.java
@@ -3,8 +3,6 @@ package app.modele;
 import java.util.ArrayList;
 
 public class Jeu {
-
-	private Manche[] manches= {};
 	
 	private ArrayList<Tour> defenses;
 	
@@ -12,19 +10,19 @@ public class Jeu {
 	
 	private Environnement env;
 	
-	private ArrayList<Ennemi> vague1 ;
+	private ArrayList<Ennemi> vague0 = new ArrayList();
 	
-	private ArrayList<Ennemi>  vague2;
+	private ArrayList<Ennemi>  vague1 = new ArrayList();
 	
-	private ArrayList<Ennemi> vague3;
+	private ArrayList<Ennemi> vague2 = new ArrayList();
 	
-	private ArrayList<Ennemi> vague4 ;
+	private ArrayList<Ennemi> vague3= new ArrayList() ;
 	
-	private ArrayList<Ennemi> vague5;
+	private ArrayList<Ennemi> vague4 = new ArrayList();
 	
 	private ArrayList<ArrayList<Ennemi>> vagues = new ArrayList<ArrayList<Ennemi>>(); //{/*vague1,vague2,vague3,vague4,vague5*/} */ 
 	
-	private ArrayList<Ennemi> ennemisEnJeu;
+	private ArrayList<Ennemi> ennemisEnJeu = new ArrayList();
 	
 	public Jeu(Environnement e) {
 		
@@ -33,62 +31,66 @@ public class Jeu {
 		env=new Environnement(carte);
 		
 		ennemisEnJeu=new ArrayList();
-		
-		
-		
-
 	}
 	
 	public void initialiserVagues(int choix) {
-		
+		vagues.add(vague0);
+		vagues.add(vague1);
+		vagues.add(vague2);
+		vagues.add(vague3);
+		 vagues.add(vague4);
 		switch(choix) {
 		
 		 case 0:
-			 for(int i = 0; i<3 ;i++) 
-				 vague1.add(new Zombie(env));
-			 vagues.add(vague1);
+			 for(int i = 0; i<3 ;i++) {
+				 vague0.add(new Zombie(env));
+			 }
+			 
 		 case 1:
-			 for(int i = 0; i<5; i++)
-				 vague2.add(new Zombie(env));
-				 vague2.add(new Squelette(env));
-			vagues.add(vague2);
+			 for(int i = 0; i<5; i++) {
+				 vague1.add(new Zombie(env));
+				 vague1.add(new Squelette(env));
+				 vagues.add(vague0);
+			 }
 		 case 2:
 			 for(int i = 0; i<8; i++){
-				 vague3.add(new Zombie(env));
-				 vague3.add(new Squelette(env));
+				 vague2.add(new Zombie(env));
+				 vague2.add(new Squelette(env));
 				 if(i<3) 
-					 vague3.add(new Pigman(env));
+					 vague2.add(new Pigman(env));
 			 }
-			 vagues.add(vague3);
+			 
 		 case 3:
 			 for(int i = 0; i<12; i++) {
-				 vague4.add(new Zombie(env));
-				 vague4.add(new Squelette(env));
-				 if(i<5)
-					 vague4.add(new Pigman(env));
+				vague3.add(new Zombie(env));
+				 vague3.add(new Squelette(env));
+				 if(i<5) {
+					vague3.add(new Pigman(env));
+				 }
 			  }
-			 vagues.add(vague4);
+			 
 		 case 4:
 			 for(int i = 0; i<15; i++) {
-				 vague5.add(new Zombie(env));
-				 vague5.add(new Squelette(env));
+				 vague4.add(new Zombie(env));
+				 vague4.add(new Squelette(env));
 				 if(i<8) {
-					 vague5.add(new Pigman(env));
+					 vague4.add(new Pigman(env));
 				 }
 				 if(i<3) 
-					 vague5.add(new Sorciere(env));
+					 vague4.add(new Sorciere(env));
 			 }
-			 vague5.add(new Wither(env));
-			 vagues.add(vague5);
+			 vague4.add(new Wither(env));
+			
 		}	
 		
 		
 	}
 	
 	public void vague(int choix) {
-		ennemisEnJeu.add(vagues.get(choix).get(0));
-		vagues.get(choix).remove(vagues.get(choix).get(0));
-		
+		if(vagues.get(choix).size() !=0) {
+			ennemisEnJeu.add(vagues.get(choix).get(0));
+			vagues.get(choix).remove(vagues.get(choix).get(0));
+		}
 		
 	}
 	
@@ -96,20 +98,21 @@ public class Jeu {
 		return ennemisEnJeu;
 	}
 	
-	public Carte getCarte() {
-		return carte;
-	}
-	
-	public Environnement getEnvironnement() {
-		return env;
-	}
-	
 	public ArrayList<Ennemi> getVague(int choix) {
 		
 		return vagues.get(choix);
 		
 	}
+
+	public Carte getCarte() {
+		return carte;
+	}
+
+	public Environnement getEnvironnement() {
+		return env;
+	}
 	
 	
 
 }
+
