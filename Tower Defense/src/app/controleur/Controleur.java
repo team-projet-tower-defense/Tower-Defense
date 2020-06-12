2a942e7d071fb083958c4b31ce6364c93d4325e0
MrPion314 <59640484+MrPion314@users.noreply.github.com>
2020-06-12 11:56:15 +0200

    Add files via upload
    Placement tour

--- Tower Defense/src/app/controleur/Controleur.java
+++ Tower Defense/src/app/controleur/Controleur.java
@@ -2,7 +2,6 @@ package app.controleur;
 
 import java.awt.TextField;
 import java.net.URL;
-import java.util.Random;
 import java.util.ResourceBundle;
 import app.modele.*;
 import app.vue.*;
@@ -11,12 +10,19 @@ import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.TilePane;
 import javafx.util.Duration;
 
 public class Controleur implements Initializable {
-
+	
+	private static final int Environnement = 0;
+	private Jeu jeu;
 	private CarteVue terrainVue;
 	private Carte carte;
 	private Sorciere ennemi;
@@ -29,60 +35,67 @@ public class Controleur implements Initializable {
 	private Catapulte tour3;
 	private Mage tour4;
 	
+	public TourVue tourvue;
 	private TourVue tourvue1;
 	private TourVue tourvue2;
 	private TourVue tourvue3;
 	private TourVue tourvue4;
 	
 	private Environnement environnement;
-  
+	
+    @FXML
+    private Button testClick;
+	@FXML
+	private Label message;
 	@FXML
    private TextField compteurVie;
-	
-
 	@FXML
     private TilePane decor;
 	@FXML
 	private Pane pane;
 	private Timeline gameLoop;
 	private int time;
-	@Override
+	private Tour tour;
+	private CarteVue carte2;
+	//private int codeCase;
 	
+	public String nompor = "non";
+	
+    @FXML
+    public ImageView defenseArcher;
+//    @FXML
+//    public ImageView Catapulte;
+//    @FXML
+//    public ImageView Chevalier;
+//    @FXML
+//    public ImageView Mage;
+  
+//    @FXML
+//    public ImageView nomTour = Archer;
+   // public int idTour = 0;
+    
+	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		
+
 		carte = new Carte();
 		environnement = new Environnement(carte);
 		
 		terrainVue = new CarteVue(carte, decor);
 		terrainVue.afficherCarte();
 		
-		ennemi = new Sorciere(5, 448, environnement);	
+		ennemi = new Sorciere(environnement);	
 		ennemivue = new EnnemiVue(ennemi, pane);
 		ennemivue.creerSpriteEnnemi();
 		
-		zombie = new Zombie(5, 416, environnement);	
+		zombie = new Zombie(environnement);	
 		zombievue = new EnnemiVue(zombie, pane);
 		zombievue.creerSpriteEnnemi();
-		
-		tour1 = new Chevalier(50, 380, environnement);
-		tour2 = new Archer(18, 380, environnement);
-		tour3 = new Catapulte(89, 380, environnement);
-		tour4 = new Mage(150, 380, environnement);
-		
-		tourvue1 = new TourVue(tour1, pane);
-		tourvue2 = new TourVue(tour2, pane);
-		tourvue3 = new TourVue(tour3, pane);
-		tourvue4 = new TourVue(tour4, pane);
-		
-		tourvue1.creerSpriteTour();
-		tourvue2.creerSpriteTour();
-		tourvue3.creerSpriteTour();
-		tourvue4.creerSpriteTour();
-		
+
+		clicTourelle();
 		initialiserGameLoop();
 		gameLoop.play();
 	}
-	
+
 	private void initialiserGameLoop() {
 		this.gameLoop = new Timeline();
 		this.gameLoop.setCycleCount(Timeline.INDEFINITE);
@@ -91,14 +104,42 @@ public class Controleur implements Initializable {
 		(ev -> {
 				ennemi.seDeplace(1);
 				zombie.seDeplace(2);
-				ennemivue.deplacementSprite();	
 				
+				ennemivue.deplacementSprite();	
 				zombievue.deplacementSprite();
 		}));
 		gameLoop.getKeyFrames().add(frame);
 	}
-    	
-	private void creerSpriteTour(Tour t) {
-		//pane.getChildren().add(t.getSprite());
+	
+	public void clicTourelle() {
+		defenseArcher.setOnMouseClicked(e ->{clicSurMap(e);});		
 	}
+	
+	public void clicSurMap(MouseEvent ev) {		
+		for (int i = 0; i < decor.getChildren().size(); i++) {
+			if(decor.getChildren().get(i).getId() == "stone" /*&& verifiePlaceLibre(decor.getChildren().get(i).getLayoutX(), decor.getChildren().get(i).getLayoutY())*/){
+				decor.getChildren().get(i).setOnMouseClicked(e ->{
+					Node tuile = (Node) e.getSource();
+					//System.out.println(jeu.getDefense());
+					ImageView tourelle = new ImageView("file:Tower Defense/src/app/ressources/archer.png");
+			//		jeu.getDefense().add(new Archer(environnement));
+					tourelle.setLayoutX(tuile.getLayoutX());
+					tourelle.setLayoutY(tuile.getLayoutY());					
+		            pane.getChildren().add(tourelle);
+				});	
+			}
+		}
+	}
+
+
+	
+/*	public boolean verifiePlaceLibre(double x, double y) {
+        for (int i = 0; i < decor.getChildren().size(); i++) {
+            if (this.e1.getTourelles().get(i).getX() == x && this.e1.getTourelles().get(i).getY() == y) {
+                return false;
+            }
+        }
+        return true;
+    }*/
+
 }
